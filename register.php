3ed30f7a7b363f5d05cc681899235def80712943
faisal1210 <75827493+faisal1210@users.noreply.github.com>
2020-12-31 14:56:41 +0700

    [faisal] modify register.php

--- register.php
+++ register.php
@@ -41,7 +41,7 @@ if(isset($_POST['register'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Register Pesbuk</title>
+    <title>Register Sosial Media/title>
 
     <link rel="stylesheet" href="css/bootstrap.min.css" />
 </head>
